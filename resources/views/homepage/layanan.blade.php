460d59c6dd819c368ba12ed743e89158b54856e4
aleWaskitha <aleiawaskitha06@gmail.com>
2025-10-31 23:18:38 +0700

    Update homepage layanan

--- resources/views/homepage/layanan.blade.php
+++ resources/views/homepage/layanan.blade.php
@@ -5,10 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>WarasWaris</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <style>
+        .layanan {
+            display: flex;
+            align-items: center;
+            gap: 20px;
+        }
+    </style>
 </head>
 <body>
     <h1>Layanan kesehatan yang lebih nyaman dan lebih dekat dengan anda</h1>
     <p>Kami memahami pentingnya pelayanan kesehatan yang cepat, mudah, dan dapat diandalkan. Dengan sistem digital dan dokter berpengalaman, kami hadir untuk memberikan pengalaman berobat yang lebih praktis dan efisien tanpa mengorbankan kenyamanan Anda.</p>
-    
+    <!-- Layanan 1 -->
+    <div class="layanan">
+        <i class="fa-solid fa-stethoscope fa-2xl" style="color: #5a81fa;"></i>
+        <p>Dokter kami siap memberikan diagnosis dan penanganan terbaik dengan pendekatan yang ramah serta terpercaya.</p>
+    </div>
+    <!-- Layanan 2 -->
+    <div class="layanan">
+        <i class="bi bi-geo-alt" style="color: #5a81fa;"></i>
+        <p>Klinik kami mudah dijangkau dan dilengkapi fasilitas yang nyaman untuk menunjang kebutuhan pasien.</p>
+    </div>
+    <!-- Layanan 3 -->
+    <div class="layanan">
+        Icon
+        <p>Pesan jadwal konsultasi Anda secara online, dapatkan nomor antrean otomatis, dan datang sesuai waktu tanpa menunggu lama.</p>
+    </div>
 </body>
 </html>
