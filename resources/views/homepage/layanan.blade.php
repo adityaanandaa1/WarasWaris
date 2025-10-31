358bd328ae140a3f21585d0c2057d75fd96611ab
aleWaskitha <aleiawaskitha06@gmail.com>
2025-10-31 22:00:07 +0700

    Tambah homepage layanan

---
+++ resources/views/homepage/layanan.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WarasWaris</title>
+</head>
+<body>
+    <h1>Layanan kesehatan yang lebih nyaman dan lebih dekat dengan anda</h1>
+    <p>Kami memahami pentingnya pelayanan kesehatan yang cepat, mudah, dan dapat diandalkan. Dengan sistem digital dan dokter berpengalaman, kami hadir untuk memberikan pengalaman berobat yang lebih praktis dan efisien tanpa mengorbankan kenyamanan Anda.</p>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('homepage.home');
 });
+
+Route::get('/layanan', function () {
+    return view('homepage.layanan');
+});
