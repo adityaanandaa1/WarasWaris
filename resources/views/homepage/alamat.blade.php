78f26edf6ebb6acc0e342624df9e31fd242f5b6c
aleWaskitha <aleiawaskitha06@gmail.com>
2025-11-03 19:38:47 +0700

    Homepage alamat - finish

--- resources/views/homepage/alamat.blade.php
+++ resources/views/homepage/alamat.blade.php
@@ -18,6 +18,10 @@
             display: flex;
         }
 
+        #subjudul {
+            padding-left: 15px;
+        }
+
         h1 {
             font-family: 'Poppins', sans-serif;
             font-size: 32px;
@@ -36,20 +40,23 @@
             display: flex;
         }
 
-        #subjudul p {
+        #teks {
             font-family: 'Poppins', sans-serif;
             font-size: 21px;
             color: #464646;
+            padding-left: 90px;
+            padding-right: 110px;
         }
 
-        #subjudul {
-            padding-left: 100px;
+        #mapsframe {
+            box-shadow:0 4px 16px 0 rgba(0,0,0,0.6)
         }
 
-        #maps p {
+        #maps p{
             font-family: 'Poppins', sans-serif;
             font-size: 15px;
             color: #5A81FA;
+            text-align: center;
         }
     </style>
 </head>
@@ -62,12 +69,12 @@
         </div>
     </div>
     <div id="lokasi">
-        <div id="subjudul">
+        <div id="teks">
             <p>Klinik kami berlokasi strategis di pusat Kecamatan Kaliwates, Jember. Alamat: Jl. Bromocor No. 2, Kepatihan, Kalisat, Kab. Jember, Jawa Timur 68193.</p>
             <p>Lokasi mudah dijangkau dengan kendaraan pribadi maupun transportasi umum. Pasien juga dapat menggunakan Google Maps untuk navigasi langsung ke klinik kami.</p>
         </div>
         <div id="maps">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d758251.2525875802!2d110.05828114108525!3d-7.716638213679846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6bf07904e2813%3A0xe6a3d376ea4e071!2sDr.Muh.Abd.Waris!5e0!3m2!1sid!2sid!4v1762161211936!5m2!1sid!2sid" width="432" height="411" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe id="mapsframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d758251.2525875802!2d110.05828114108525!3d-7.716638213679846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6bf07904e2813%3A0xe6a3d376ea4e071!2sDr.Muh.Abd.Waris!5e0!3m2!1sid!2sid!4v1762161211936!5m2!1sid!2sid" width="432" height="411" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <p>Klik pada peta untuk membuka arah menuju Klinik Dr. Muh. Abd. Waris di Google Maps.</p>
         </div>
     </div>
