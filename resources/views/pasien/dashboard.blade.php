a569e254659a3580eb6ef097db529e7c260be3e8
cindyfaizanabila <cindyfaizanabila@gmail.com>
2025-11-04 13:46:41 +0700

    db pasyen

--- resources/views/pasien/dashboard.blade.php
+++ resources/views/pasien/dashboard.blade.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Dashboard pasien</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body{
+            font-family: 'Poppins', sans-serif;
+        }
+        .halaman {
+            font-size: 27px;
+            font-weight: 700px;
+            color:#464646;
+        }
+        .tanggal{
+            font-size: 16px;
+            font-weight: 400;
+        }
+        .card1{
+            color:white;
+            font-size: 16px;
+            background-color:#5A81FA;
+        }
+
+    </style>
 </head>
 <body>
-    <h1>INI DASHBOARD PASIEN</h1>
+
+    <div class="halaman">Dashboard</div>
+    <div class="tanggal">{{ \Carbon\Carbon::now()->format('d F Y') }}</div>
+
+    <div class="card1">
+        <p><b>Selamat datang!</b></p>
+        <p>Konsultasikan kesehatanmu sekarang juga!</p>
+    </div>
+
 </body>
 </html>
