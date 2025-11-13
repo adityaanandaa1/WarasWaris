d1dfe8276df456d1e3568d6320d83470927102d3
aleWaskitha <aleiawaskitha06@gmail.com>
2025-11-13 11:21:55 +0700

    Update dashboard dokter - Modal Atur Jadwal

--- resources/views/layouts/dokter.blade.php
+++ resources/views/layouts/dokter.blade.php
@@ -8,6 +8,7 @@
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     
     <!-- Font Google -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -42,6 +43,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/todaydate.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="{{ asset('js/datepicker.js') }}"></script>
 </body>
 </html>
