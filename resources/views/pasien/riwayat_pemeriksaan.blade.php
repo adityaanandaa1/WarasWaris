7e17a14b89ba74ba8febc4b477eae4112d8eec0a
cindyfaizanabila <cindyfaizanabila@gmail.com>
2025-12-05 10:11:34 +0700

    rekam medis

--- resources/views/pasien/riwayat_pemeriksaan.blade.php
+++ resources/views/pasien/riwayat_pemeriksaan.blade.php
@@ -8,10 +8,7 @@
             <i class="ri-arrow-left-line"></i>
         </a>
         <h1 class="medicalrecord-title">Rekam Medis Elektronik</h1>
-    </div>
-    <div class="medicalrecord-header-inner">
-        <div class="medicalrecord-wrapper">
-            <div class="medicalrecord-action-buttons">
+             <div class="medicalrecord-action-buttons">
                 <button class="medicalrecord-btn-print" onclick="window.print()">
                     <svg width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z" clip-rule="evenodd"/>
@@ -19,6 +16,10 @@
                     Print / PDF
                 </button>
             </div>
+    </div>
+    <div class="medicalrecord-header-inner">
+        <div class="medicalrecord-wrapper">
+       
             <div class="medicalrecord-rm-section">
                 <svg width="114" height="72" viewBox="0 0 134 92" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                     <rect x="24.9834" width="84.0339" height="84.0339" fill="url(#pattern0_328_878)"/>
